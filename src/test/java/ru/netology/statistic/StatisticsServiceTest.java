d4e57c1828ab9ca9f9e59d6f25768c00807c7493
Antonina Yurchenko <tonya2512@yandex.ru>
2023-09-09 00:35:27 +0300

    add extra test and setup

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
-}
+
+        @Test
+        void findMaxInTheEnd() {
+            StatisticsService service = new StatisticsService();
+
+            long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 125};
+            long expected = 125;
+
+            long actual = service.findMax(incomesInBillions);
+
+            Assertions.assertEquals(expected, actual);
+        }
+    }
